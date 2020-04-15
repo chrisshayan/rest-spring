f6112e4c1d1694883839764a357561958accaf2b
chrisshayan <hamedshayan@gmail.com>
2020-04-15 19:11:07 +0700

    Initial commit

---
+++ src/test/java/com/chrisshayan/spring/example/restspring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisshayan.spring.example.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
